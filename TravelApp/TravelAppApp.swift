eac9e708e50e34c8c2ad2874076425b35e254dcc
katefu <fuyuehaokate@163.com>
2023-08-13 15:49:38 -0400

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by KateFu on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
